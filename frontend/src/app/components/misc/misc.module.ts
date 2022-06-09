66a49d855de0e6e1e06ddaecbd346b354952b1a3
Devin Harris <devinharris.316@gmail.com>
2022-06-08 21:47:57 -0400

    Moved sidebar slider component

--- frontend/src/app/components/misc/misc.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { ImageStackComponent } from './image-stack/image-stack.component';
-import { ImageStackModule } from './image-stack/image-stack.module';
-
-@NgModule({
-  imports: [CommonModule, ImageStackModule],
-})
-export class MiscModule {}
