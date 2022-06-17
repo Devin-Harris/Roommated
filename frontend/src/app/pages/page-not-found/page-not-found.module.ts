ba30bd066c1e8f7d885e55b9bbe1a4102eab8aa8
Alex Yang <yangal@mail.uc.edu>
2022-06-17 17:01:55 +0000

    Merge PR 12 - Created 404 page
    - Created page for 404 error (page not found)
    
    Related work items: #16

--- frontend/src/app/pages/page-not-found/page-not-found.module.ts
+++ frontend/src/app/pages/page-not-found/page-not-found.module.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { ImageStackModule } from 'src/app/components/misc/image-stack/image-stack.module';
+import { NavigationModule } from 'src/app/components/navigation/navigation.module';
+import { FooterbarModule } from 'src/app/components/navigation/footer-bar/footer-bar.module';
 
 @NgModule({
-  declarations: [
-    PageNotFoundComponent
-  ],
-  exports: [
-    PageNotFoundComponent
-  ],
+  declarations: [PageNotFoundComponent],
+  imports: [CommonModule, RouterModule, ImageStackModule, FooterbarModule],
+  exports: [PageNotFoundComponent],
 })
 export class PageNotFoundModule { }
