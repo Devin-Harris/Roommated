bf568fcf92d162dcd1311c5694ef54b4c2c14e74
Alex205993 <yangal@mail.uc.edu>
2022-06-28 11:51:14 -0400

    Sign in page and group info template components

--- frontend/src/app/pages/signin-page/signin-page.component.ts
+++ frontend/src/app/pages/signin-page/signin-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'signin-page',
   templateUrl: './signin-page.component.html',
   styleUrls: ['./signin-page.component.scss'],
 })
-export class SignInPageComponent {}
+export class SignInPageComponent {
+}
