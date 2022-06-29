457f036cd5b664f95c7cc45d8068e10d69f661d0
Alex205993 <yangal@mail.uc.edu>
2022-06-29 14:56:58 -0400

    Sign in text field ts

--- frontend/src/app/pages/signin-page/signin-page.component.ts
+++ frontend/src/app/pages/signin-page/signin-page.component.ts
@@ -7,4 +7,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./signin-page.component.scss'],
 })
 export class SignInPageComponent {
+
+  password = ""
+  email = ""
+
+  handleEmailChange(e: any){
+    this.email = e.target.value;
+  }
+
+  handlePasswordChange(e: any){
+    this.password = e.target.value;
+  }
+
+  signIn(){
+    
+  }
 }
