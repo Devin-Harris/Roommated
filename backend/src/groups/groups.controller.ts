0f5a762dd11d73b63e6c2c33dbfdf66d3a5eb767
Avery Brinkman <brinkmae@mail.uc.edu>
2022-07-11 11:58:03 -0400

    groups controller cleanup

--- backend/src/groups/groups.controller.ts
+++ backend/src/groups/groups.controller.ts
@@ -7,48 +7,30 @@ import {
   Param,
   Post,
   Put,
-  Query,
   Req,
+  Request,
+  UnauthorizedException,
 } from '@nestjs/common';
-import {
-  UpdateGroupsDto,
-  ResponseGroupDto,
-  UpdateGroupPayloadDto,
-  CreateGroupDto,
-} from '@rmtd/common/dtos';
+import { ResponseGroupDto, UpdateGroupPayloadDto, CreateGroupDto } from '@rmtd/common/dtos';
 import { GroupsService } from './groups.service';
 import { ApiNotFoundResponse, ApiOkResponse } from '@nestjs/swagger';
 import { DeleteResult } from 'typeorm';
+import { Role } from 'src/authentication/roles/roles.decorator';
+import { AuthRole } from '@rmtd/common/enums';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('groups')
 export class GroupsController {
-  constructor(private readonly groupsService: GroupsService) {}
+  constructor(
+    private readonly groupsService: GroupsService,
+    private readonly usersService: UsersService,
+  ) {}
 
-  @Get()
+  @Get('/me')
   @ApiOkResponse({ type: ResponseGroupDto, isArray: true })
   @ApiNotFoundResponse()
-  async findByIds(@Query('ids') idsString: string): Promise<ResponseGroupDto[]> {
-    const ids = idsString.split(',').map((id) => parseInt(id));
-    const groups = await this.groupsService.findByIds(ids);
-
-    if (groups.length < ids.length) {
-      const notFoundIds = [];
-      ids.forEach((id) => {
-        if (!groups.some((group) => group.id === id)) {
-          notFoundIds.push(id);
-        }
-      });
-      throw new NotFoundException(`Could not find groups with the ids: ${notFoundIds}`);
-    }
-
-    return this.groupsService.mapGroupsToResponseDto(groups);
-  }
-
-  @Get('/user/:id')
-  @ApiOkResponse({ type: ResponseGroupDto, isArray: true })
-  @ApiNotFoundResponse()
-  async findByUserId(@Param('id') userId: number): Promise<ResponseGroupDto> {
-    const group = await this.groupsService.findByUserId(userId);
+  async findByUserId(@Req() req): Promise<ResponseGroupDto> {
+    const group = await this.groupsService.findByUserId(req.user.id);
     return this.groupsService.mapGroupToResponseDto(group);
   }
 
@@ -61,52 +43,33 @@ export class GroupsController {
     throw new NotFoundException();
   }
 
-  @Post()
+  @Post('/me')
   @ApiOkResponse({ type: ResponseGroupDto, isArray: true })
-  async makeGroups(@Body() body: CreateGroupDto, @Req() req: any): Promise<ResponseGroupDto> {
-    // TODO: do authentication check to get user id to set as createUserId of groups to be made
-    const createUser = req.user || {
-      id: 1,
-    };
+  async makeGroups(@Body() body: CreateGroupDto, @Request() req: any): Promise<ResponseGroupDto> {
+    const createUser = await this.usersService.findById(req.user.id);
     const group = await this.groupsService.createGroup(body, createUser);
     return this.groupsService.mapGroupToResponseDto(group);
   }
 
-  @Put()
-  @ApiOkResponse({ type: ResponseGroupDto, isArray: true })
-  @ApiNotFoundResponse()
-  async updateGroups(@Body() body: UpdateGroupsDto): Promise<ResponseGroupDto[]> {
-    // TODO: do authentication check to make sure user making update is admin of site or is owner/admin of group
-    const groups = await this.groupsService.updateByIds(body);
-    return this.groupsService.mapGroupsToResponseDto(groups);
-  }
-
-  @Put(':id')
+  @Role(AuthRole.GroupAdmin)
+  @Put('/me')
   @ApiOkResponse({ type: ResponseGroupDto })
   @ApiNotFoundResponse()
   async updateGroupById(
-    @Param() id: number,
+    @Request() req,
     @Body() body: UpdateGroupPayloadDto,
   ): Promise<ResponseGroupDto> {
-    // TODO: do authentication check to make sure user making update is admin of site or is owner/admin of group
+    const usersGroup = await this.groupsService.findByUserId(req.user.id);
+    if (usersGroup.id != body.mutatedGroup.id) throw new UnauthorizedException();
     const group = await this.groupsService.update(body);
     return this.groupsService.mapGroupToResponseDto(group);
   }
 
-  @Delete()
+  @Role(AuthRole.GroupOwner)
+  @Delete('/me')
   @ApiOkResponse({ type: DeleteResult })
   @ApiNotFoundResponse()
-  async deleteByIds(@Query('ids') idsString: string): Promise<DeleteResult> {
-    // TODO: do authentication check to make sure user making update is admin of site or is owner/admin of group
-    const ids = idsString.split(',').map((id) => parseInt(id));
-    return await this.groupsService.deleteByIds(ids);
-  }
-
-  @Delete()
-  @ApiOkResponse({ type: DeleteResult })
-  @ApiNotFoundResponse()
-  async deleteById(@Param('id') id: number): Promise<DeleteResult> {
-    // TODO: do authentication check to make sure user making update is admin of site or is owner/admin of group
-    return await this.groupsService.deleteById(id);
+  async deleteById(@Request() req): Promise<DeleteResult> {
+    return await this.groupsService.deleteById(req.user.groupId);
   }
 }
