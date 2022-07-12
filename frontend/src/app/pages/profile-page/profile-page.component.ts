8bb58742ebe251fa5a3b6f68b10f3d30de4f2068
Devin Harris <devinharris.316@gmail.com>
2022-07-12 19:35:16 -0400

    Cleaned up heading for profile pages

--- frontend/src/app/pages/profile-page/profile-page.component.ts
+++ frontend/src/app/pages/profile-page/profile-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { User } from '@rmtd/common/interfaces';
-import { Observable } from 'rxjs';
+import { Observable, Subject, take, takeUntil } from 'rxjs';
 import { profilePageLoaded, selectUserProfile } from 'src/app/state/profile';
 
 @Component({
@@ -10,22 +10,33 @@ import { profilePageLoaded, selectUserProfile } from 'src/app/state/profile';
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss'],
 })
-export class ProfilePageComponent implements OnInit {
+export class ProfilePageComponent implements OnInit, OnDestroy {
   $profileUser: Observable<User | null>;
 
   id: Number | null = null;
 
+  profileName = '';
+
+  private $destroyed = new Subject<void>();
+
   constructor(private store: Store, private route: ActivatedRoute) {
-    this.route.params.subscribe((params) => {
+    this.route.params.pipe(takeUntil(this.$destroyed)).subscribe((params) => {
       if (params['id'] !== undefined) {
         this.id = params['id'];
       }
     });
 
     this.$profileUser = this.store.select(selectUserProfile);
+    this.$profileUser.pipe(takeUntil(this.$destroyed)).subscribe((profileUser) => {
+      this.profileName = profileUser?.firstname + ' ' + profileUser?.lastname;
+    });
   }
 
   ngOnInit(): void {
     this.store.dispatch(profilePageLoaded({ id: this.id }));
+  }
+
+  ngOnDestroy(): void {
+    this.$destroyed.next();
   }
 }
