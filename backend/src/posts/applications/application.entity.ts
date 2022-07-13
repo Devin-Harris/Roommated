3dcb72da6d60084e9db3cc84a340f6dc149e6aae
Avery Brinkman <brinkmae@mail.uc.edu>
2022-07-13 14:23:59 -0400

    created entity

---
+++ backend/src/posts/applications/application.entity.ts
@@ -0,0 +1,30 @@
+import { GroupInvitationState } from '@rmtd/common/enums';
+import { Application as ApplicationInterface } from '@rmtd/common/interfaces';
+import { User } from 'src/users/users.entity';
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Application implements ApplicationInterface {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unsigned: true })
+  postId: number;
+
+  // @OneToOne(() => Post)
+  // @JoinColumn()
+  // post: Post;
+
+  @Column({ unsigned: true })
+  applicantUserId: number;
+
+  @OneToOne(() => User)
+  @JoinColumn()
+  applicantUser: User;
+
+  @Column({ nullable: true })
+  comment: string;
+
+  @Column()
+  state: GroupInvitationState;
+}
---
+++ shared/interfaces/Application.ts
@@ -0,0 +1,12 @@
+import { GroupInvitationState } from '../enums';
+import { User } from './User';
+// import { Post } from './Post';
+
+export interface Application {
+  postId: number;
+  // post: Post;
+  applicantUserId: number;
+  applicantUser: User;
+  comment?: string;
+  state: GroupInvitationState;
+}
