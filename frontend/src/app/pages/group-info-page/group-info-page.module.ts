4db214d8c9863d77eae1206d6b3ecd96ae79931d
Devin Harris <devinharris.316@gmail.com>
2022-07-14 20:49:13 -0400

    Fixed import and console warning

--- frontend/src/app/pages/group-info-page/group-info-page.module.ts
+++ frontend/src/app/pages/group-info-page/group-info-page.module.ts
@@ -3,12 +3,12 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CreateGroupFormModule } from 'src/app/components/forms/create-group-form/create-group-form.module';
-import { MyGroupSidebarModule } from 'src/app/components/misc/my-group-sidebar/my-group-sidebar.module';
 import { TabGroupModule } from 'src/app/components/misc/tab-group/tab-group.module';
 import { AuthenticationInterceptor } from 'src/app/state/authentication/AuthenticationInterceptor.service';
 import { GroupModule } from 'src/app/state/group/group.module';
 import { GroupInfoPageComponent } from './group-info-page.component';
 import { UserAvatarModule } from 'src/app/components/icons/user-avatar/user-avatar.module';
+import { MyGroupSidebarModule } from 'src/app/components/sidebar-slider/my-group-sidebar/my-group-sidebar.module';
 
 @NgModule({
   declarations: [GroupInfoPageComponent],
