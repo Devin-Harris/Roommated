f95ae07e130407f834801a88519c6eb2b2d985c5
Avery Brinkman <brinkmae@mail.uc.edu>
2022-07-18 16:48:15 -0400

    reworked dto and interface

--- shared/dtos/posts/Application.dto.ts
+++ shared/dtos/posts/Application.dto.ts
@@ -1,17 +1,100 @@
 import { Application, Post, User } from '../../interfaces';
-import { Expose } from 'class-transformer';
+import { Expose, Exclude, Type } from 'class-transformer';
+import { IsEnum, IsNotEmpty, IsOptional, IsString, IsNumber } from 'class-validator';
 import { GroupInvitationState } from '../../enums';
+import { ResponseUserDto } from '../users';
+import { ResponsePostDto } from './Post.dto';
 
-export class ApplicationDto implements Partial<Application> {
+export class BaseApplicationDto implements Application {
+  @IsNotEmpty()
+  @IsNumber()
+  @Expose()
+  id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
   @Expose()
   postId: number;
 
+  @IsNotEmpty()
+  @Type((post) => ResponsePostDto)
+  @Expose()
+  post: Post;
+
+  @IsNotEmpty()
+  @IsNumber()
   @Expose()
   applicantUserId: number;
 
+  @IsNotEmpty()
+  @Type((applicantUser) => ResponseUserDto)
   @Expose()
-  comment?: string | undefined;
+  applicantUser: User;
 
+  @IsOptional()
+  @IsString()
+  @Expose()
+  comment?: string;
+
+  @IsNotEmpty()
+  @IsEnum(GroupInvitationState)
   @Expose()
   state: GroupInvitationState;
+}
+
+export class CreateApplicationDto implements Partial<Application> {
+  @IsNotEmpty()
+  @IsNumber()
+  @Expose()
+  postId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Expose()
+  applicantUserId: number;
+
+  @IsOptional()
+  @IsString()
+  @Expose()
+  comment?: string;
+
+  @IsNotEmpty()
+  @IsEnum(GroupInvitationState)
+  @Expose()
+  state: GroupInvitationState;
+}
+
+export class UpdateApplicationDto implements Partial<Application> {
+  @IsNotEmpty()
+  @IsNumber()
+  @Expose()
+  id: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Expose()
+  postId: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Expose()
+  applicantUserId: number;
+
+  @IsOptional()
+  @IsString()
+  @Expose()
+  comment: string;
+
+  @IsOptional()
+  @IsEnum(GroupInvitationState)
+  @Expose()
+  state: GroupInvitationState;
+}
+
+export class ResponseApplicationDto extends BaseApplicationDto {
+  @Exclude()
+  postId: number;
+
+  @Exclude()
+  applicantUserId: number;
 }
--- shared/interfaces/Application.ts
+++ shared/interfaces/Application.ts
@@ -3,11 +3,11 @@ import { User } from './User';
 import { Post } from './Post';
 
 export interface Application {
-  id: number;
-  postId: number;
-  post: Post;
-  applicantUserId: number;
-  applicantUser: User;
+  id?: number;
+  postId?: number;
+  post?: Post;
+  applicantUserId?: number;
+  applicantUser?: User;
   comment?: string;
-  state: GroupInvitationState;
+  state?: GroupInvitationState;
 }
