708d86ce29d63bf26e17265444d79b2193a0b9ed
Avery Brinkman <brinkmae@mail.uc.edu>
2022-07-20 17:42:08 -0400

    cleanup

--- backend/src/posts/applications/application.module.ts
+++ backend/src/posts/applications/application.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from 'src/users/users.module';
-import { PostModule } from '../post.module';
 import { ApplicationController } from './application.controller';
 import { Application } from './application.entity';
 import { ApplicationService } from './application.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Application]), PostModule, UsersModule],
+  imports: [TypeOrmModule.forFeature([Application])],
   controllers: [ApplicationController],
   providers: [ApplicationService],
 })
