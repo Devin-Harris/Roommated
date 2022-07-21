8a7c6e1c500a9b8b889dc60605e73c1e2e62ba89
Devin Harris <devinharris.316@gmail.com>
2022-07-21 17:54:40 -0400

    Corrected application state

--- frontend/src/app/state/application/application.state.ts
+++ frontend/src/app/state/application/application.state.ts
@@ -2,21 +2,9 @@ import { Gender, PostParkingFilter, PostPetFilter, PostTypeFilter } from '@rmtd/
 import { Post, PostFilter } from '@rmtd/common/interfaces';
 
 export interface ApplicationState {
-  filters: PostFilter;
-  filteredPosts: Post[];
   error: Error | null;
 }
 
 export const initialState: ApplicationState = {
-  filters: {
-    mapCenterLat: undefined,
-    mapCenterLng: undefined,
-    mapZoom: 13,
-    housingTypes: [PostTypeFilter.Any],
-    pets: [PostPetFilter.Any],
-    parkings: [PostParkingFilter.Any],
-    genders: [Gender.Any],
-  },
-  filteredPosts: [],
   error: null,
 };
